1380d9bb81512517593b5ad044ffec62a32eb1bf
Josh <joshconstantinelim@gmail.com>
2024-05-17 02:15:37 +1000

    algorithmRun play button and speed slider

--- src/app/visualisers/graphs/algorithms/algorithmRun.tsx
+++ src/app/visualisers/graphs/algorithms/algorithmRun.tsx
@@ -13,42 +13,95 @@ import {
   FastArrowRight,
   NavArrowLeft,
   NavArrowRight,
+  Pause,
+  Play,
 } from "iconoir-react";
 
 export default function AlgorithmRun() {
   const alg = useAlgorithm();
 
+  const advanceAlgorithmInterval = useRef<NodeJS.Timeout | null>(null);
+  const [play, setPlay] = useState<boolean>(false);
+  const [playDelay, setPlayDelay] = useState<number>(1000);
+
+  const playTimeoutCb = useCallback(() => {
+    if (!alg.algorithm) return;
+
+    alg.next();
+
+    // if at end of steps, stop playing
+    if (alg.stepIndex >= (alg.steps?.length ?? Infinity) - 1) {
+      setPlay(false);
+      return;
+    }
+
+    // set timeout for next step
+    advanceAlgorithmInterval.current = setTimeout(playTimeoutCb, playDelay);
+  }, [alg, playDelay]);
+
+  useEffect(() => {
+    if (play) {
+      if (advanceAlgorithmInterval.current)
+        clearTimeout(advanceAlgorithmInterval.current);
+      advanceAlgorithmInterval.current = setTimeout(playTimeoutCb, playDelay);
+    } else {
+      if (advanceAlgorithmInterval.current)
+        clearTimeout(advanceAlgorithmInterval.current);
+    }
+  }, [play, playDelay, playTimeoutCb]);
+
   if (!alg.algorithm) return <></>;
 
   return (
-    <div className="space-y-5 py-10">
-      <div className="flex flex-row overflow-hidden rounded-full">
-        <button
+    <motion.div layout className="space-y-5 py-10">
+      <motion.div
+        layout
+        transition={{ duration: 0.1, damping: 20, stiffness: 150 }}
+        className="flex flex-row rounded-full shadow-sm"
+      >
+        <motion.button
+          layout
+          transition={{ duration: 0.1, damping: 20, stiffness: 150 }}
           className={cn(
-            "flex flex-row gap-1 border-r border-white bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600",
+            "flex flex-row gap-1 rounded-l-full border-r border-slate-100 bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600",
             alg.stepIndex <= 0 &&
               "cursor-not-allowed bg-gray-400 hover:bg-gray-400",
           )}
           onClick={alg.start}
         >
           <FastArrowLeft />
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          layout
+          transition={{ duration: 0.1, damping: 20, stiffness: 150 }}
           className={cn(
-            "flex flex-row gap-1 border-r border-white bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600",
+            "flex flex-row gap-1 border-r border-slate-100 bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600",
             alg.stepIndex <= 0 &&
               "cursor-not-allowed bg-gray-400 hover:bg-gray-400",
           )}
           onClick={alg.prev}
         >
           <NavArrowLeft />
-        </button>
-        {/* <button className="flex flex-row gap-1 bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-lime-600">
-          <Play />
-        </button> */}
-        <button
+        </motion.button>
+        <motion.button
+          layout
+          transition={{ duration: 0.1, damping: 20, stiffness: 150 }}
+          className="flex flex-row gap-1 bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600"
+          onClick={() => {
+            if (!alg.steps) return;
+            if (alg.stepIndex >= alg.steps.length - 1) return;
+
+            setPlay((prev) => !prev);
+          }}
+        >
+          {play ? <Pause /> : <Play />}
+        </motion.button>
+        {play && <PlayDelaySlider setPlayDelay={setPlayDelay} />}
+        <motion.button
+          layout
+          transition={{ duration: 0.1, damping: 20, stiffness: 150 }}
           className={cn(
-            "flex flex-row gap-1 border-l border-white bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600",
+            "flex flex-row gap-1 border-l border-slate-100 bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600",
             alg.steps &&
               alg.stepIndex >= alg.steps.length - 1 &&
               "cursor-not-allowed bg-gray-400 hover:bg-gray-400",
@@ -56,10 +109,12 @@ export default function AlgorithmRun() {
           onClick={alg.next}
         >
           <NavArrowRight />
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          layout
+          transition={{ duration: 0.1, damping: 20, stiffness: 150 }}
           className={cn(
-            "flex flex-row gap-1 border-l border-white bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600",
+            "flex flex-row gap-1 rounded-r-full border-l border-slate-100 bg-green-500 px-5 py-3 text-lg transition-colors hover:bg-green-600",
             alg.steps &&
               alg.stepIndex >= alg.steps.length - 1 &&
               "cursor-not-allowed bg-gray-400 hover:bg-gray-400",
@@ -67,15 +122,63 @@ export default function AlgorithmRun() {
           onClick={alg.end}
         >
           <FastArrowRight />
-        </button>
-      </div>
+        </motion.button>
+      </motion.div>
       {alg.steps && <RunSlider alg={alg} />}
+    </motion.div>
+  );
+}
+
+function PlayDelaySlider({
+  setPlayDelay,
+}: {
+  setPlayDelay: (val: number) => void;
+}) {
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const y = useMotionValue(sliderRef.current?.clientHeight ?? 0);
+
+  useMotionValueEvent(y, "change", (latest) => {
+    if (!sliderRef.current) return;
+    console.log(latest, sliderRef.current.clientHeight);
+    console.log(latest / sliderRef.current.clientHeight);
+    setPlayDelay(
+      2000 * Math.pow(latest / sliderRef.current.clientHeight, 3) + 100,
+    );
+  });
+
+  useEffect(() => {
+    y.set((sliderRef.current?.clientHeight ?? 0) / 2);
+  }, [y]);
+
+  return (
+    <div
+      ref={sliderRef}
+      className="relative w-[9px] border-l border-slate-100 bg-blue-400"
+      onClick={(e) => {
+        y.set(
+          e.clientY -
+            (sliderRef.current?.getBoundingClientRect().y ?? e.clientX),
+        );
+      }}
+    >
+      <motion.div
+        drag="y"
+        dragConstraints={{
+          top: 0,
+          bottom: sliderRef.current?.clientHeight ?? 0,
+        }}
+        dragElastic={false}
+        dragMomentum={false}
+        style={{ y, x: "-0%", translateY: "-50%" }}
+        whileHover={{ scale: 1.3 }}
+        className="absolute left-[-50%] top-0 h-4 w-4 cursor-move rounded-full bg-white shadow-lg"
+      ></motion.div>
     </div>
   );
 }
 
 function RunSlider({ alg }: { alg: UseAlgorithmNotNull }) {
-  const showStepOfMilliseconds = 1000;
+  const showStepOfMilliseconds = 2200;
   const sliderRef = useRef<HTMLDivElement>(null);
 
   const [showStepOf, setShowStepOf] = useState(false);
@@ -111,19 +214,16 @@ function RunSlider({ alg }: { alg: UseAlgorithmNotNull }) {
       setTimeout(() => setShowStepOf(false), showStepOfMilliseconds),
     );
 
-    if (!dragging) return;
+    if (!dragging || !alg.steps || !sliderRef.current) return;
 
-    const len = alg.steps?.length ?? Infinity;
+    const len = alg.steps.length;
     const newIndex = clamp(
       0,
-      Math.floor(
-        (latest / (sliderRef.current?.clientWidth ?? Infinity)) *
-          (alg.steps?.length ?? 0),
-      ),
-      (alg.steps?.length ?? 0) - 1,
+      Math.floor((latest / sliderRef.current.clientWidth) * alg.steps.length),
+      alg.steps.length - 1,
     );
     alg.setStepIndex(newIndex);
-    x.set((newIndex / len) * (sliderRef.current?.clientWidth ?? 0));
+    x.set((newIndex / len) * sliderRef.current.clientWidth);
   });
 
   // don't render if the steps have not been calculated
@@ -154,7 +254,7 @@ function RunSlider({ alg }: { alg: UseAlgorithmNotNull }) {
       <motion.button
         className="absolute top-[50%] h-5 w-5 cursor-move rounded-full bg-white shadow-lg"
         drag="x"
-        style={{ translateY: "-50%", x }}
+        style={{ translateY: "-50%", x, translateX: "-50%" }}
         dragConstraints={{
           left: 0,
           right: sliderRef.current?.clientWidth ?? 0,
